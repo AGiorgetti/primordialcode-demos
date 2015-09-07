a25239b11bea9ec20b6ee4e8021ef12d4c56eea1
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2015-09-07 13:33:36 +0200

    fist commit, some typescript samples added

---
+++ ts/app.ts
@@ -0,0 +1,7 @@
+var a2: number = 0;
+
+function display(text: string): void {
+	console.log(text);
+}
+
+display("Alessandro");
---
+++ tsgulp/file1.ts
@@ -0,0 +1,9 @@
+/// <reference path="file2.ts"/>
+
+function display1(text: string): void {
+	console.log(text);
+} 
+
+display1("Alessandro");
+
+display2("Giorgetti");
